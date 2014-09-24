a8fa42d7eae668e37f9a74c29b947affc0be3b71
Robberiffic <ericrob815@gmail.com>
2014-09-24 15:23:24 -0400

    Created vectors, began parsing loop
    Added vectors for time and membrane voltage
    
    Added initial values for gating variables
    
    Began to implement for loop to parse and calculate values for n,m,h and
    conductances.

--- AxonSimulation.m
+++ AxonSimulation.m
@@ -16,7 +16,21 @@ I_K = n.^4.*gK.*h.*(Vm-eK);
 I_L = gL.*(Vm-eL);
 I_ion = I-I_K-I_Na-I_L;
 
-% Gating Variables
+
+% Derivatives
+dVm = I_ion./Cm;
+dm = alpha_m.*(1-m)-beta_m.*m;
+dn = alpha_n.*(1-n)-beta_n.*n;
+dh = alpha_h.*(1-h)-beta_h.*h;
+Euler's Method
+y_n1 = y_n + h.*ff;
+
+% Create time vector
+t = linspace(t_0, delta_t, t_f);
+% Create Vm vector to correspond to time vector
+%
+
+% Calculate Gating Variables from Vm Vector
 alpha_m = .1.*((25-Vm)./(exp((25-Vm)/10)-1));
 beta_m = 4.*exp(-Vm./18);
 alpha_n = .1.*((10-Vm)./(exp((10-Vm)/10)-1));
@@ -24,13 +38,19 @@ beta_n = 125.*exp(-Vm./80);
 alpha_h = .07.*exp(-Vm./20);
 beta_h = 1./(exp((30-Vm)./10)+1);
 
-% Derivatives
-dVm = I_ion./Cm;
-dm = alpha_m.*(1-m)-beta_m.*m;
-dn = alpha_n.*(1-n)-beta_n.*n;
-dh = alpha_h.*(1-h)-beta_h.*h;
-% Euler's Method
-y_n1 = y_n + h.*ff;
+% Set intial conditions for gating variables
+% n0 =
+% m0 =
+% h0 =
+
+% Parse through every element of the time/membrane voltage vectors,
+% calculating m,n,h, conductances, and currents
+
+for ind = 0:length(t)
+
+    
+    
+end
 
 
 
